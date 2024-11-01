0e417e6bac727e831d5fc60ef6d8ac7bf14852d5
RasmusCodes <rasmus.suonio@gmail.com>
2024-11-01 03:50:38 +0200

    1.5 done!

--- part1/courseInfo/src/App.jsx
+++ part1/courseInfo/src/App.jsx
@@ -1,35 +1,59 @@
 const App = () => {
-  const course = 'Half Stack application development'
-  const part1 = 'Fundamentals of React'
-  const exercises1 = 10
-  const part2 = 'Using props to pass data'
-  const exercises2 = 7
-  const part3 = 'State of a component'
-  const exercises3 = 14
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7
+      },
+      {
+        name: 'State of a component',
+        exercises: 14
+      }
+    ]
+  }
+  
+  // Defining the Header function
   const Header = ({course}) => {
     return (
-    <h1>{course}</h1>
+    <h1>{course.name}</h1>
     )
   }
-  const Content = ({part1, exercises1, part2, exercises2, part3, exercises3}) => {
+  // Defining Part function to use it in Content
+  const Part = ({part, exercises}) => {
+    return (
+      <p>{part} {exercises}</p>
+    )
+  }
+  // Defining Content using Part
+  const Content = ({parts}) => {
     return (
       <div>
-        <p> {part1} {exercises1}</p>
-        <p> {part2} {exercises2}</p>
-        <p> {part3} {exercises3}</p>
+        <Part part={parts[0]} exercises={parts[1]} />
+        <Part part={parts[2]} exercises={parts[3]} />
+        <Part part={parts[4]} exercises={parts[5]} />
       </div>
     )
   }
+  // Calculate the total by knowing the indices of the values. 
   const Total = ({a, b, c}) => {
     return (
-      <p>Number of exercises: {a + b + c}</p>
+      <p>Number of exercises: {parts[1] + parts[3] + parts[5]}</p>
     )
   }
+
+  // Defining this array this way so that the code works.
+  const parts = [course.parts[0].name, course.parts[0].exercises, course.parts[1].name, course.parts[1].exercises, course.parts[2].name, course.parts[2].exercises]
+
   return (
     <div>
       <Header course={course} />
-      <Content part1={part1} exercises1={exercises1} part2={part2} exercises2={exercises2} part3={part3} exercises3={exercises3} />
-      <Total a={exercises1} b={exercises2} c={exercises3} />
+      <Content parts={parts} />
+      <Total parts={parts} />
     </div>
   )
 }
